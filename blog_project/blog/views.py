f92ffee57f9780d41759b18b67772aeeb7e160c1
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-15 22:53:43 +0200

    made simple changes to test how I can show multiple blogs on the same page

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+posts = [
+    {
+        'author': 'Musa Mvuna',
+        'title': 'First Blog',
+        'content': 'My first ever blog content',
+        'date_posted': 'May 4, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Second Blog',
+        'content': 'My second blog content',
+        'date_posted': 'May 5, 2024'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Third Blog',
+        'content': 'My third blog content',
+        'date_posted': 'May 6, 2024'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html')
