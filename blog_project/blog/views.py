37742e4c9179e8ccd522643f3e26a8d8554782cb
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-26 21:11:45 +0200

    attemped to add registration page but recieved an error - 25min mark

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 
-posts = [
-    {
-        'author': 'Musa Mvuna',
-        'title': 'First Blog',
-        'content': 'My first ever blog content',
-        'date_posted': 'May 4, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Second Blog',
-        'content': 'My second blog content',
-        'date_posted': 'May 5, 2024'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Third Blog',
-        'content': 'My third blog content',
-        'date_posted': 'May 6, 2024'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
